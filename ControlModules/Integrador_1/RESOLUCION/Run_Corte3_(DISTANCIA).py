6fe2452be9ba5b488638fe060d65feef382d3e5a
santi2208 <santi2208@gmail.com>
2018-09-15 13:09:34 -0300

    Agrego movimiento de servo al Integrador de distancia

--- ControlModules/Integrador_1/RESOLUCION/Run_Corte3_(DISTANCIA).py
+++ ControlModules/Integrador_1/RESOLUCION/Run_Corte3_(DISTANCIA).py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 from DistanceActions import *
+from Actions import *
 
 trigger = 24
 echo = 23
@@ -11,16 +12,18 @@ maxDistance = 30
 ledRojo = 14
 ledAmarillo = 12 
 ledVerde = 16
+servo = 5
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(ledRojo, GPIO.OUT)
 GPIO.setup(ledAmarillo, GPIO.OUT)
 GPIO.setup(ledVerde, GPIO.OUT)
 
 def Main():
     try:    
+        lastColor = 'green'
         distanceActions = DistanceActions(trigger, echo)
+        servoActions = Actions()
         while True:
             distance = distanceActions.TakeDistance()
             print(distance)
@@ -28,15 +31,24 @@ def Main():
                 GPIO.output(ledRojo, GPIO.HIGH)
                 GPIO.output(ledAmarillo, GPIO.LOW)
                 GPIO.output(ledVerde, GPIO.LOW)
+                if(lastColor != 'red'):
+                    servoActions.Deg0(servo)
+                lastColor = 'red'
             else:
                 if(distance >= minDistance and distance <= midDistance):
                     GPIO.output(ledRojo, GPIO.LOW)
                     GPIO.output(ledAmarillo, GPIO.HIGH)
                     GPIO.output(ledVerde, GPIO.LOW)
+                    if(lastColor != 'yellow'):
+                        servoActions.Deg90(servo)
+                    lastColor = 'yellow'
                 else:
                     GPIO.output(ledRojo, GPIO.LOW)
                     GPIO.output(ledAmarillo, GPIO.LOW)
                     GPIO.output(ledVerde, GPIO.HIGH)
+                    if(lastColor != 'green'):
+                        servoActions.Deg180(servo)
+                    lastColor = 'green'
             time.sleep(0.3)
     except (KeyboardInterrupt, SystemExit):
         raise
